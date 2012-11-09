f202389d242c1c2fcaee3ddba3bf70b5dd205b9b
Deni Husni FR <Deni Husni FR@TrainerPositron>
2012-11-09 10:05:50 +0700

    Commit Awal Menggunakan Maven

---
+++ TrainingCenterDHFR/src/main/java/com/epicentrum/dhfr/trainingcenter/DAO/BaseDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epicentrum.dhfr.trainingcenter.DAO;
+
+/**
+ *
+ * @author Deni Husni FR
+ */
+public interface BaseDAO {
+    
+}
