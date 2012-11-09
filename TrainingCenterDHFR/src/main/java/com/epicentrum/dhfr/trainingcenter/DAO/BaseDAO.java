268e4d57280fdbbbb445c51f799527a929c141f4
Deni Husni FR <Deni Husni FR@TrainerPositron>
2012-11-09 10:12:37 +0700

    Commit Awal Menggunakan Maven

--- TrainingCenterDHFR/src/main/java/com/epicentrum/dhfr/trainingcenter/DAO/BaseDAO.java
+++ TrainingCenterDHFR/src/main/java/com/epicentrum/dhfr/trainingcenter/DAO/BaseDAO.java
@@ -4,10 +4,25 @@
  */
 package com.epicentrum.dhfr.trainingcenter.DAO;
 
+import java.util.List;
+import org.hibernate.criterion.Order;
+
 /**
  *
  * @author Deni Husni FR
  */
-public interface BaseDAO {
-    
+public interface BaseDAO <T>{
+     public T get(String kode);
+
+    public void save(T entity);
+
+    public void update(T entity);
+
+    public void delete(T entity);
+
+    public List<T> getAllData(int firstResult, int maxResults, Order order);
+
+    public Long getTotalCount();
+
+    public List<T> getAll();
 }
